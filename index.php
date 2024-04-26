0cc0b6ced54b71c36d1fc2521fa90422d14ccb72
João Castro <jcastro@reloading.biz>
2024-04-26 20:27:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Press Start 2P' rel='stylesheet'>
+    <link rel="stylesheet" href="public/styles/index.css">
+    <link rel="icon" href="public/images/logo.png">
+    <script src=""></script>
+    <title>Pixel Web</title>
+</head>
+<body>
+    <header class="menu" id="Menu">
+        <h1>Pixel Web</h1>
+        <a class="loginBtn" href="pages/login.php">INICIAR SESSÃO</a>
+    </header>
+    <section class="titleContainer">
+        <h1>Jogos</h1>
+    </section>
+    <section class="gameContainer">
+        <div class="testgame">
+            <div class="game">
+                afaffa
+            </div>
+            <div class="game">
+                afaffa
+            </div>
+            <div class="game">
+                afaffa
+            </div>
+            <div class="game">
+                afaffa
+            </div>
+            <div class="game">
+                afaffa
+            </div>
+            <div class="game">
+                afaffa
+            </div>
+            <div class="game">
+                afaffa
+            </div>
+            <div class="game">
+                afaffa
+            </div>
+            <div class="game">
+                afaffa
+            </div>
+            <div class="game">
+                afaffa
+            </div>
+        </div>
+    </section>
+</body>
+</html>
